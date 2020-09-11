2b157301c587f93d449f04bb90abe46181f773f4
Patrick Lannigan <plannigan@wayfair.com>
2020-09-11 17:25:40 -0400

    Remove debug output

--- .github/actions/install-dependencies/action.sh
+++ .github/actions/install-dependencies/action.sh
@@ -3,8 +3,6 @@ set -euo pipefail
 echo "Ensuring pip is up to date"
 python -m pip install --upgrade pip
 
-env | sort
-
 if [[ "${INSTALL_REQUIREMENTS}" == "true"  ]]; then
   echo "Installing code requirements"
   pip install -r requirements.txt
