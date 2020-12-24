953ded275a18395c77fb6cb77dfe69f01706411d
Patrick Lannigan <plannigan@wayfair.com>
2020-12-24 13:10:35 -0500

    Add support for pre-release versions
    Also:
    * Switch back to original branch after committing
    * Correct final message of script

--- docker/bump_version.sh
+++ docker/bump_version.sh
@@ -3,18 +3,57 @@
 set -eo pipefail
 
 DEFAULT_BRANCH="main"
+PRE_RELEASE_ARG="pre_release"
+
+DEV_RELEASE="false"
+PART=""
 
 function usage() {
-  echo "usage: bump_version.sh major|minor|patch"
+  echo "usage: bump_version.sh [--dev] pre-release|major|minor|patch"
   echo ""
-  echo " major|minor|patch : Part of version string to updated."
+  echo " --dev             : Update to a dev pre-release for the new version. Can't be used with pre-release."
+  echo " pre-release       : Update pre-release component for the current version."
+  echo " major|minor|patch : Part of version string to updated for a final release."
   exit 1
 }
 
-if [[ $# != 1 || ! "${1}" =~ major|minor|patch ]]; then
+while [[ $# -gt 0 ]]; do
+  arg="$1"
+  case $arg in
+  --dev)
+    DEV_RELEASE="true"
+    ;;
+  -h | --help)
+    usage
+    ;;
+  pre-release | major | minor | patch)
+    if [[ "${PART}" != "" ]]; then
+      echo "Part has already been given"
+      usage
+    fi
+    PART="${arg}"
+    if [[ "${PART}" == "pre-release" ]]; then
+      # part names can't have dashes, but dashes are better for a CLI
+      PART="${PRE_RELEASE_ARG}"
+    fi
+    ;;
+  "")
+    # ignore
+    ;;
+  *)
+    echo "Unexpected argument: ${arg}"
+    usage
+    ;;
+  esac
+  shift
+done
+
+if [[ "${PART}" == "" ]]; then
+  echo "Part to update not given"
   usage
-else
-  PART="${1}"
+elif [[ "${PART}" == "${PRE_RELEASE_ARG}" && "${DEV_RELEASE}" == "true" ]]; then
+  echo "pre-release and --dev can not be used at the same time"
+  usage
 fi
 
 CURRENT_BRANCH=$(git branch --show-current)
@@ -29,15 +68,22 @@ elif [[ "$CURRENT_STATUS" != "" ]]; then
   echo "Commit or stash the changes before running a version bump."
   exit 3
 fi
-# Ensure on default branch
-git switch --no-guess main
+
 # Capture value of new version
 NEW_VERSION=$(bump2version --dry-run --list "${PART}" | grep new_version | sed -r s,"^.*=",,)
+# bump2version currently makes it hard increment a part and start in a dev state
+# https://github.com/c4urself/bump2version/issues/63
+if [[ "${DEV_RELEASE}" == "true" ]]; then
+  NEW_VERSION="${NEW_VERSION}-dev"
+fi
+
 # Create bump branch
 BUMP_BRANCH_NAME="bump_version_to_${NEW_VERSION}"
-git switch -c "${BUMP_BRANCH_NAME}"
+echo "Creating new branch ${BUMP_BRANCH_NAME}"
+echo
+git switch --quiet -c "${BUMP_BRANCH_NAME}"
 # Update files
-bump2version "${PART}"
+bump2version --new-version "${NEW_VERSION}" "${PART}"
 
 # Updating the changelog has to be done manually
 #   - bump2version doesn't support inserting dates https://github.com/c4urself/bump2version/issues/133
@@ -51,5 +97,8 @@ git add CHANGELOG.md
 git commit --amend --no-edit
 # Show effected files
 git show --pretty="" --name-only
+# Back to original branch
+git switch --quiet "${DEFAULT_BRANCH}"
 
-echo "Run 'git push --set-upstream origin ${BUMP_BRANCH_NAME}' to "
+echo
+echo "Run 'git push --set-upstream origin ${BUMP_BRANCH_NAME}' to create a pull request"
