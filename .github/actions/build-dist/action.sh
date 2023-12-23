30b95081bcf98da96ca2990fb8c60b5d91f50c29
Patrick Lannigan <p.lannigan@gmail.com>
2023-12-23 14:33:21 -0500

    Multiple CI improvements
    * pin CI package versions
    * tell renovate about pinned versions
    * quote shell variables
    * build released package from sdist

--- .github/actions/build-dist/action.sh
+++ .github/actions/build-dist/action.sh
@@ -1,8 +1,8 @@
 set -euo pipefail
 
 echo "Ensuring pip is up to date"
-python -m pip install --upgrade pip
+python -m pip install --upgrade pip==23.3.2
 echo "Installing the latest version of pypa/build"
-pip install build
+pip install build==1.0.3
 
-python -m build --sdist --wheel --outdir dist/ .
+python -m build --outdir dist/ .
